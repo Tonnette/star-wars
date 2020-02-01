26d5f9f5eddb1a06711d5b747f4b6af89ab5d0c6
Tonnette Stanford <tonnettestanford@yahoo.com.au>
2020-02-01 12:39:48 +1100

    updated port

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -14,29 +15,16 @@ app.use(express.json());
 
 // Star Wars Characters (DATA)
 // =============================================================
-var characters = [
+var reservation = [
   {
-    routeName: "yoda",
-    name: "Yoda",
-    role: "Jedi Master",
-    age: 900,
-    forcePoints: 2000
-  },
-  {
-    routeName: "darthmaul",
-    name: "Darth Maul",
-    role: "Sith Lord",
-    age: 200,
-    forcePoints: 1200
-  },
-  {
-    routeName: "obiwankenobi",
-    name: "Obi Wan Kenobi",
-    role: "Jedi Master",
-    age: 55,
-    forcePoints: 1350
+    routeName: "ton",
+    name: "Ton",
+    phone: "998",
+    email: "ton@ton.com",
+    id: 55
   }
 ];
+
 
 // Routes
 // =============================================================
